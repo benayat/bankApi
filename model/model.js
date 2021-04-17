1298c2fd4d8719344e7bbc6e6fc32821ef3e30ee
Benaya Trabelsi <benaya7@gmail.com>
2021-04-17 15:40:33 +0300

    setting bankapi to mvc

--- model/model.js
+++ model/model.js
@@ -1,22 +1,26 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+
 const pathDB = path.join(__dirname, '../model/clients.json');
 
-const getAllClients = () => {
+const createClientHelper = (id) => {
+  return updateClientHelper(id, 0, 0, 'post');
+};
+
+const getAllClientsHelper = () => {
   const rawData = fs.readFileSync(pathDB);
   return JSON.parse(rawData);
 };
-const getClient = (id) => {
-  const clients = getAllClients();
+
+const getClientHelper = (id) => {
+  const clients = getAllClientsHelper();
   return clients.find((client) => client.id === id);
 };
-const createClient = (id) => {
-  return updateClient(id, 0, 0, 'post');
-};
-const updateClient = (id, cash, credit, method) => {
+
+const updateClientHelper = (id, cash, credit, method) => {
   const client = { id, cash, credit };
   console.log(client);
-  const clients = getAllClients();
+  const clients = getAllClientsHelper();
   const indexToUpdate = clients.findIndex((client) => client.id === id);
   if (indexToUpdate === -1 && method !== 'post') {
     throw Error('the item you want to update is not there!');
@@ -33,33 +37,33 @@ const updateClient = (id, cash, credit, method) => {
   fs.writeFileSync(pathDB, JSON.stringify(clients));
   return client;
 };
-const updateCredit = (id, credit) => {
-  const client = getClient(id);
-  return updateClient(id, client.cash, credit, 'put');
+const updateCreditHelper = (id, credit) => {
+  const client = getClientHelper(id);
+  return updateClientHelper(id, client.cash, credit, 'put');
 };
-const withdraw = (id, cash) => {
-  const client = getClient(id);
+const withdrawHelper = (id, cash) => {
+  const client = getClientHelper(id);
   cash = parseFloat(cash);
   if (cash > client.cash + client.credit) {
     throw Error("can't withdraw, not enough cash");
   } else {
     client.cash -= cash;
   }
-  return updateClient(id, client.cash, client.credit, 'put');
+  return updateClientHelper(id, client.cash, client.credit, 'put');
 };
-const deposit = (id, cash) => {
-  const client = getClient(id);
+const depositHelper = (id, cash) => {
+  const client = getClientHelper(id);
   cash = parseFloat(cash);
-  return updateClient(id, client.cash + cash, client.credit, 'put');
+  return updateClientHelper(id, client.cash + cash, client.credit, 'put');
 };
 //returns client2 if all is ok.
-const transfer = (idFrom, idTo, cash) => {
-  const res1 = withdraw(idFrom, cash);
-  const res2 = deposit(idTo, cash);
+const transferHelper = (idFrom, idTo, cash) => {
+  const res1 = withdrawHelper(idFrom, cash);
+  const res2 = depositHelper(idTo, cash);
   return res1 && res2;
 };
-const deleteClient = (id) => {
-  const clients = getAllClients();
+const deleteClientHelper = (id) => {
+  const clients = getAllClientsHelper();
   const indexToDelete = clients.findIndex((client) => client.id === id);
   if (indexToDelete === -1) throw Error("client doesn't exist in the database");
   const result = clients.splice(indexToDelete, 1);
@@ -67,11 +71,12 @@ const deleteClient = (id) => {
   fs.writeFileSync(pathDB, JSON.stringify(clients));
 };
 module.exports = {
-  getAllClients,
-  createClient,
-  updateCredit,
-  withdraw,
-  deposit,
-  transfer,
-  deleteClient,
+  createClientHelper,
+  getAllClientsHelper,
+  updateClientHelper,
+  updateCreditHelper,
+  withdrawHelper,
+  depositHelper,
+  transferHelper,
+  deleteClientHelper,
 };
